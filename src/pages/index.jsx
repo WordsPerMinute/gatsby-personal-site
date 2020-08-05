32da762daa735405f28c95e0665b9274016c94d8
WordsPerMinute <git@coreyhodge.net>
2020-08-04 23:45:40 -0600

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Home() {
+  return (
+
+    <div style={{ color: `purple` }}>
+      {/* highlight-start */}
+      <h1>Hello Gatsby!</h1>
+      <p>What a world.</p>
+      {/* highlight-start */}
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+ 
+  );
+}
