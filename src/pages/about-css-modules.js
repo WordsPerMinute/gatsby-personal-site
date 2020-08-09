ad6b57569adf229d34e24ff0a049b8686e98a568
WordsPerMinute <git@coreyhodge.net>
2020-08-09 17:35:00 -0600

    installed Typography plugin

--- src/pages/about-css-modules.js
+++ src/pages/about-css-modules.js
@@ -19,6 +19,11 @@ export default function About() {
     <Container>
       <h1>About CSS Modules</h1>
       <p>CSS Modules are cool</p>
+      <User
+        username="Jane Doe"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+        excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
       <User 
         username="Bob Smith"
         avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
